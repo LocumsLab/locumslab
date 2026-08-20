fb8deddb31fcb5324d4a7083ea2dcc0b1cb00570
LocumsLab <support@crnapathway.com>
2026-08-20 10:30:40 -0400

    Delete netlify/functions/score-one.js

--- netlify/functions/score-one.js
+++
@@ -1,9 +0,0 @@
-const { scoreContract } = require('./score-contract');
-const rubric = require('./rubric-v1.json');
-const s = scoreContract(require('./' + process.argv[2]), rubric);
-console.log('Grade:', s.overall.letter, s.overall.pct + '%', s.overall.provisional ? '(PROVISIONAL)' : '', '| scored ' + s.overall.fieldsScored + '/' + s.overall.fieldsTotal);
-console.log('Categories:', Object.entries(s.categories).map(([k,v]) => k + '=' + (v.letter || 'insufficient data')).join('  '));
-console.log('\nPriorities:');
-s.priorities.forEach(p => console.log(' ' + p.rank + '. ' + p.label + ' [' + p.priority + '] - ' + p.currentTerm));
-console.log('\nStrengths:', s.strengths.map(f => f.label).join(', ') || 'none');
-console.log('\nClarify (' + s.clarifications.length + '):', s.clarifications.map(c => c.label + (c.unsupportedValue ? '*' : '') + (c.relatedText ? '+' : '')).join(', '));
